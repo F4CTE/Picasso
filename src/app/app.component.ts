1836a3296adf1ac1cf6c1b29819f41732ea19c3a
facte sun <shems.chelgoui@gmail.com>
2023-06-19 09:45:05 +0200

    yes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { ITask } from './ITask';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Picasso';
+  title = 'ToDoAppPart1';
+  arrayTasks: ITask[] = [
+    { id: 1, name: 'boire' },
+    { id: 2, name: 'manger' },
+    { id: 3, name: 'dormir' }
+  ]
 }
